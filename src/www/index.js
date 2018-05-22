2e3d2d614bb627bdb8d4bb3e918116d1cc962db4
Alex Kocharin <alex@kocharin.ru>
2018-05-22 17:18:54 +0000

    Switch to bootstrap 4

--- src/www/index.js
+++ src/www/index.js
@@ -1,14 +1,71 @@
 import utils from '../utils';
-import './bootstrap_custom/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 const serializeError = require('serialize-error');
 const { ungzip } = require('pako/lib/inflate');
 
 const $ = global.$ = global.jQuery = require('jquery');
+const { JSONForm } = require('json-form');
 
-require('json-form');
+/* eslint-disable max-len */
+JSONForm.fieldTemplate = function (inner) {
+  return '<div class="<%= cls.groupClass %> jsonform-node jsonform-error-<%= keydash %>' +
+    '<%= elt.htmlClass ? " " + elt.htmlClass : "" %>' +
+    '<%= (node.required && node.formElement && (node.formElement.type !== "checkbox") ? " jsonform-required" : "") %>' +
+    '<%= (node.isReadOnly() ? " jsonform-readonly" : "") %>' +
+    '<%= (node.disabled ? " jsonform-disabled" : "") %>' +
+    '" data-jsonform-type="<%= node.formElement.type %>">' +
+    '<% if (node.title && !elt.notitle && elt.inlinetitle !== true) { %>' +
+      '<label class="<%= cls.labelClass %>" for="<%= node.id %>"><%= node.title %></label>' +
+    '<% } %>' +
+    '<div class="<%= cls.controlClass %>">' +
+      '<% if (node.prepend || node.append) { %>' +
+        '<div class="<%= node.prepend ? cls.prependClass : "" %> ' +
+        '<%= node.append ? cls.appendClass : "" %>">' +
+        '<% if (node.prepend && node.prepend.indexOf("<button ") >= 0) { %>' +
+          '<% if (cls.buttonAddonClass) { %>' +
+            '<span class="<%= cls.buttonAddonClass %>"><%= node.prepend %></span>' +
+          '<% } else { %>' +
+            '<%= node.prepend %>' +
+          '<% } %>' +
+        '<% } %>' +
+        '<% if (node.prepend && node.prepend.indexOf("<button ") < 0) { %>' +
+          '<span class="<%= cls.addonClass %>"><%= node.prepend %></span>' +
+        '<% } %>' +
+      '<% } %>' +
+      inner +
+      '<% if (node.append && node.append.indexOf("<button ") >= 0) { %>' +
+        '<% if (cls.buttonAddonClass) { %>' +
+          '<span class="<%= cls.buttonAddonClass %>"><%= node.append %></span>' +
+        '<% } else { %>' +
+          '<%= node.append %>' +
+        '<% } %>' +
+      '<% } %>' +
+      '<% if (node.append && node.append.indexOf("<button ") < 0) { %>' +
+        '<span class="<%= cls.addonClass %>"><%= node.append %></span>' +
+      '<% } %>' +
+      '<% if (node.prepend || node.append) { %>' +
+        '</div>' +
+      '<% } %>' +
+      '<% if (node.description) { %>' +
+        '<span class="form-text text-muted small jsonform-description"><%= node.description %></span>' +
+      '<% } %>' +
+      '<span class="form-text text-muted small jsonform-errortext" style="display:none;"></span>' +
+    '</div></div>';
+};
 
+JSONForm.elementTypes.fieldset.template =
+  '<fieldset class="jsonform-node jsonform-error-<%= keydash %> <% if (elt.expandable) { %>expandable<% } %> <%= elt.htmlClass?elt.htmlClass:"" %>" ' +
+  '<% if (id) { %> id="<%= id %>"<% } %>' +
+  ' data-jsonform-type="fieldset">' +
+  '<% if (node.title || node.legend) { %><legend><%= node.title || node.legend %></legend><% } %>' +
+  '<% if (elt.expandable) { %><div class="<%= cls.groupClass %>"><% } %>' +
+  '<%= children %>' +
+  '<% if (elt.expandable) { %></div><% } %>' +
+  '<span class="form-text text-muted small jsonform-errortext" style="display:none;"></span>' +
+  '</fieldset>';
+/* eslint-enable max-len */
 
 function fatalError(text) {
   let msg;
